607a05e1fdee6a7328536eaaa55ed75c789fee91
Jerry <jerryysk.stillwaters@gmail.com>
2020-06-08 18:58:54 +0400

    removed json specs since not in use anymore

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -12,7 +12,6 @@ package ifneeded discord 0.6.0 "
     source [file join $dir gateway.tcl] ;
     source [file join $dir callback.tcl] ;
     source [file join $dir disrest.tcl] ;
-    source [file join $dir json_specs.tcl] ;
     source [file join $dir disrest_channel.tcl] ;
     source [file join $dir disrest_guild.tcl] ;
     source [file join $dir disrest_invite.tcl] ;
